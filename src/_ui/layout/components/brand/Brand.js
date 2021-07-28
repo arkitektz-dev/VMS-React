b73668ffbb2ff09225a619dcf2b4b9b4b6f5a428
SyedAlamdarHussain <shahzerrr@gmail.com>
2021-07-28 12:45:29 +0500

    set default logo for application

--- src/_ui/layout/components/brand/Brand.js
+++ src/_ui/layout/components/brand/Brand.js
@@ -3,13 +3,16 @@ import { useSelector } from "react-redux";
 
 function Brand() {
   const { tenant } = useSelector((state) => state.auth);
-  let logo = tenant ? tenant.logo : "";
+  let logo = "/assets/images/logo/VMSLogo.png";
+  if (tenant && tenant.logo !== null) {
+    logo = tenant.logo;
+  }
 
   return (
     <>
       <div className="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
         <a className="sidebar-brand brand-logo" href="index.html">
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="vms logo" />
         </a>
         <a className="sidebar-brand brand-logo-mini" href="index.html">
           {/* <img src={require("../../assets/images/logo-mini.svg")} alt="logo" /> */}
